b9ec20b4ace6f2f166cd1d6b034efeaf4f654bcc
Jorge Gomez <jgomezmail-dev@proton.me>
2026-06-28 21:35:30 -0400

    adding unit tests, using sync as a starting point

--- src/commands/update.rs
+++ src/commands/update.rs
@@ -55,3 +55,273 @@ pub fn run(workspace: &impl Operations) -> Result<()> {
     Ok(())
 }
 
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use pretty_assertions::assert_eq;
+    use std::cell::Cell;
+    use std::fs;
+    use tempfile::tempdir;
+
+    // Used for stubbing out operations requiring the git backend and topdir
+    struct MockWorkspace {
+        top_dir: PathBuf,
+        git: MockGitBackend,
+    }
+
+    impl Operations for MockWorkspace {
+        fn top_dir(&self) -> &std::path::Path {
+            &self.top_dir
+        }
+        fn git(&self) -> &impl GitOperations {
+            &self.git
+        }
+    }
+
+    // Used for keeping track of how many times each git operation was called
+    struct MockGitBackend {
+        is_repo_count: Cell<u32>,
+        is_repo_return: Cell<bool>,
+        clone_count: Cell<u32>,
+        fetch_count: Cell<u32>,
+        rev_as_hash_count: Cell<u32>,
+        rev_as_hash_rev: Cell<String>,
+        rev_as_hash_return: Cell<String>,
+        checkout_count: Cell<u32>,
+    }
+
+    impl GitOperations for MockGitBackend {
+        #[allow(unused)]
+        fn is_repo(&self, repo_dir: impl AsRef<std::path::Path>) -> bool {
+            self.is_repo_count.set(self.is_repo_count.get() + 1);
+            self.is_repo_return.get()
+        }
+
+        #[allow(unused)]
+        fn rev_as_hash(&self, repo_dir: impl AsRef<std::path::Path>, rev: &str) -> Result<String> {
+            self.rev_as_hash_rev.set(String::from(rev));
+            self.rev_as_hash_count.set(self.rev_as_hash_count.get() + 1);
+            let value = self.rev_as_hash_return.take();
+            self.rev_as_hash_return.set(value.clone());
+            Ok(value)
+        }
+
+        #[allow(unused)]
+        fn clone_repo(&self, remote: &str, repo_dir: impl AsRef<std::path::Path>) -> Result<()> {
+            self.clone_count.set(self.clone_count.get() + 1);
+            Ok(())
+        }
+
+        #[allow(unused)]
+        fn fetch(&self, repo_dir: impl AsRef<std::path::Path>) -> Result<()> {
+            self.fetch_count.set(self.fetch_count.get() + 1);
+            Ok(())
+        }
+
+        #[allow(unused)]
+        fn checkout(&self, hash: &str, repo_dir: impl AsRef<std::path::Path>) -> Result<()> {
+            self.checkout_count.set(self.checkout_count.get() + 1);
+            Ok(())
+        }
+    }
+
+    impl MockGitBackend {
+        fn new() -> Self {
+            Self {
+                is_repo_count: Cell::new(0),
+                is_repo_return: Cell::new(false),
+                clone_count: Cell::new(0),
+                fetch_count: Cell::new(0),
+                rev_as_hash_count: Cell::new(0),
+                rev_as_hash_rev: Cell::new(String::new()),
+                rev_as_hash_return: Cell::new(String::new()),
+                checkout_count: Cell::new(0),
+            }
+        }
+    }
+
+    fn default_manifest() -> &'static str {
+        r#"
+    repos:
+      - name: chord
+        remote: https://github.com/JorgeG-Dev/chord
+        revision: main
+    "#
+    }
+
+    // This will fail in practice
+    fn multi_repo_manifest() -> &'static str {
+        r#"
+    repos:
+      - name: chord
+        remote: https://github.com/JorgeG-Dev/chord
+        revision: main
+      - name: chord
+        remote: https://github.com/JorgeG-Dev/chord
+        revision: main
+    "#
+    }
+
+    #[test]
+    fn test_update_clones_if_repo_missing() {
+        let dir = tempdir().unwrap();
+        fs::write(dir.path().join("chord.yaml"), default_manifest()).unwrap();
+
+        let workspace = MockWorkspace {
+            top_dir: dir.path().to_path_buf(),
+            git: MockGitBackend::new(),
+        };
+        workspace.git.is_repo_return.set(false);
+
+        run(&workspace).unwrap();
+        assert_eq!(1, workspace.git.clone_count.get());
+        assert_eq!(1, workspace.git.fetch_count.get());
+        assert_eq!(1, workspace.git.is_repo_count.get());
+        assert_eq!(1, workspace.git.rev_as_hash_count.get());
+        assert_eq!(1, workspace.git.checkout_count.get());
+    }
+
+    #[test]
+    fn test_update_creates_lockfile_if_missing() {
+        let dir = tempdir().unwrap();
+        fs::write(dir.path().join("chord.yaml"), default_manifest()).unwrap();
+
+        let workspace = MockWorkspace {
+            top_dir: dir.path().to_path_buf(),
+            git: MockGitBackend::new(),
+        };
+        workspace.git.is_repo_return.set(false);
+
+        // Just ensuring the lockfile doesn't exist
+        assert_eq!(false, dir.path().join("chord.lock.yaml").exists());
+        run(&workspace).unwrap();
+        assert_eq!(true, dir.path().join("chord.lock.yaml").exists());
+    }
+
+    #[test]
+    fn test_update_does_not_clone_if_exists() {
+        let dir = tempdir().unwrap();
+        fs::write(dir.path().join("chord.yaml"), default_manifest()).unwrap();
+
+        let workspace = MockWorkspace {
+            top_dir: dir.path().to_path_buf(),
+            git: MockGitBackend::new(),
+        };
+        workspace.git.is_repo_return.set(true);
+
+        run(&workspace).unwrap();
+        assert_eq!(0, workspace.git.clone_count.get());
+        assert_eq!(1, workspace.git.fetch_count.get());
+        assert_eq!(1, workspace.git.is_repo_count.get());
+        assert_eq!(1, workspace.git.rev_as_hash_count.get());
+        assert_eq!(1, workspace.git.checkout_count.get());
+    }
+
+    #[test]
+    fn test_missing_manifest() {
+        let dir = tempdir().unwrap();
+
+        let workspace = MockWorkspace {
+            top_dir: dir.path().to_path_buf(),
+            git: MockGitBackend::new(),
+        };
+
+        assert_eq!(true, run(&workspace).is_err());
+    }
+
+    #[test]
+    fn test_multi_update_with_clone() {
+        let dir = tempdir().unwrap();
+        fs::write(dir.path().join("chord.yaml"), multi_repo_manifest()).unwrap();
+
+        let workspace = MockWorkspace {
+            top_dir: dir.path().to_path_buf(),
+            git: MockGitBackend::new(),
+        };
+        workspace.git.is_repo_return.set(false);
+
+        run(&workspace).unwrap();
+        assert_eq!(2, workspace.git.clone_count.get());
+        assert_eq!(2, workspace.git.fetch_count.get());
+        assert_eq!(2, workspace.git.is_repo_count.get());
+        assert_eq!(2, workspace.git.rev_as_hash_count.get());
+        assert_eq!(2, workspace.git.checkout_count.get());
+    }
+
+    #[test]
+    fn test_update_does_not_use_lockfile_revision_at_all() {
+        let dir = tempdir().unwrap();
+        fs::write(dir.path().join("chord.yaml"), default_manifest()).unwrap();
+        fs::write(
+            dir.path().join("chord.lock.yaml"),
+            r#"
+repos:
+  - name: chord
+    revision: 0123456789012345678901234567890123456789
+"#,
+        )
+        .unwrap();
+        let workspace = MockWorkspace {
+            top_dir: dir.path().to_path_buf(),
+            git: MockGitBackend::new(),
+        };
+        workspace.git.is_repo_return.set(false);
+        workspace
+            .git
+            .rev_as_hash_return
+            .set(String::from("11223344"));
+
+        run(&workspace).unwrap();
+        assert_eq!("main", workspace.git.rev_as_hash_rev.take());
+    }
+
+    #[test]
+    fn test_update_with_empty_manifest() {
+        let dir = tempdir().unwrap();
+        fs::write(dir.path().join("chord.yaml"), "repos: []").unwrap();
+
+        let workspace = MockWorkspace {
+            top_dir: dir.path().to_path_buf(),
+            git: MockGitBackend::new(),
+        };
+
+        run(&workspace).unwrap();
+        assert_eq!(0, workspace.git.clone_count.get());
+        assert_eq!(0, workspace.git.fetch_count.get());
+        assert_eq!(0, workspace.git.is_repo_count.get());
+        assert_eq!(0, workspace.git.rev_as_hash_count.get());
+        assert_eq!(0, workspace.git.checkout_count.get());
+    }
+
+    #[test]
+    fn test_update_overwrites_lockfile_with_new_sha() {
+        let dir = tempdir().unwrap();
+        fs::write(dir.path().join("chord.yaml"), default_manifest()).unwrap();
+        fs::write(
+            dir.path().join("chord.lock.yaml"),
+            r#"
+repos:
+  - name: chord
+    revision: 0123456789012345678901234567890123456789
+"#,
+        )
+        .unwrap();
+
+        let workspace = MockWorkspace {
+            top_dir: dir.path().to_path_buf(),
+            git: MockGitBackend::new(),
+        };
+        workspace.git.is_repo_return.set(false);
+        workspace
+            .git
+            .rev_as_hash_return
+            .set(String::from("11223344"));
+
+        run(&workspace).unwrap();
+
+        let lockfile_contents = fs::read_to_string(dir.path().join("chord.lock.yaml")).unwrap();
+        println!("{:?}", lockfile_contents);
+        assert!(lockfile_contents.contains("11223344"));
+    }
+}
