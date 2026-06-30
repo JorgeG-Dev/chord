06989f6b7111cb77d74d486311652204c1d97aac
Jorge Gomez <jgomezmail-dev@proton.me>
2026-06-30 19:59:42 -0400

    added error printing and added matching on workspace creation

--- src/main.rs
+++ src/main.rs
@@ -8,21 +8,29 @@ use cli::{Cli, Commands};
 
 fn main() -> Result<()> {
     let args = Cli::parse();
+
     let result = match args.command {
-        Commands::Init { path } => commands::init(path)?,
-        Commands::Topdir => commands::topdir()?,
+        Commands::Init { path } => commands::init(path),
+        Commands::Topdir => commands::topdir(),
         _ => {
             let backend = workspace::GitBackend;
-            let workspace = workspace::Workspace::new(backend)?;
-            match args.command {
-                Commands::Status => commands::status(&workspace)?,
-                Commands::Sync => commands::sync(&workspace)?,
-                Commands::Update => commands::update(&workspace)?,
-                Commands::Forall { command } => commands::forall(command, &workspace)?,
-                _ => unreachable!(),
+            match workspace::Workspace::new(backend) {
+                Ok(workspace) => match args.command {
+                    Commands::Status => commands::status(&workspace),
+                    Commands::Sync => commands::sync(&workspace),
+                    Commands::Update => commands::update(&workspace),
+                    Commands::Forall { command } => commands::forall(command, &workspace),
+                    _ => unreachable!(),
+                },
+                Err(e) => Err(e),
             }
         }
     };
+
+    if let Err(e) = &result {
+        println!("Error: {}", e);
+        std::process::exit(1);
+    }
 
     Ok(())
 }
