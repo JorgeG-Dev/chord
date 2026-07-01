b36436b05abaf7c332c9e24102bbde2537b0443d
Jorge Gomez <jgomezmail-dev@proton.me>
2026-07-01 00:21:39 -0400

    reworked main to initialize workspace with topdir

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,10 @@ mod cli;
 mod commands;
 mod workspace;
 
-use anyhow::Result;
+use anyhow::{Result, anyhow};
 use clap::Parser;
 use cli::{Cli, Commands};
+use workspace::{GitBackend, Workspace, utils};
 
 fn main() -> Result<()> {
     let args = Cli::parse();
@@ -12,19 +13,20 @@ fn main() -> Result<()> {
     let result = match args.command {
         Commands::Init { path } => commands::init(path),
         Commands::Topdir => commands::topdir(),
-        _ => {
-            let backend = workspace::GitBackend;
-            match workspace::Workspace::new(backend) {
-                Ok(workspace) => match args.command {
-                    Commands::Status => commands::status(&workspace),
-                    Commands::Sync => commands::sync(&workspace),
-                    Commands::Update => commands::update(&workspace),
+        _ => match utils::get_top_dir() {
+            Some(top_dir) => {
+                let backend = GitBackend;
+                let workspace = Workspace::new(top_dir, backend);
+                match args.command {
+                    Commands::Status => commands::status(workspace),
+                    Commands::Sync => commands::sync(workspace),
+                    Commands::Update => commands::update(workspace),
                     Commands::Forall { command } => commands::forall(command, &workspace),
                     _ => unreachable!(),
-                },
-                Err(e) => Err(e),
+                }
             }
-        }
+            None => Err(anyhow!("not within chord workspace")),
+        },
     };
 
     if let Err(e) = &result {
