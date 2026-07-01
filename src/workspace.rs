4068152ddd6868c44d31f929e98ce1153db53197
Jorge Gomez <jgomezmail-dev@proton.me>
2026-07-01 15:42:08 -0400

    moved logic out of the commands function into the workspace struct

--- src/workspace.rs
+++ src/workspace.rs
@@ -1,5 +1,7 @@
 use anyhow::Result;
+use anyhow::bail;
 use std::path::{Path, PathBuf};
+use std::process::Command;
 
 mod git;
 mod lockfile;
@@ -31,13 +33,17 @@ impl Workspace {
         &self.top_dir
     }
 
-    /// Resolves the specified repo to the revision specified in its manifest
-    /// entry
-    pub fn resolve_repo(&self, mut repo: ManifestRepo) -> Result<ManifestRepo> {
-        let repo_dir = match &repo.location {
+    fn repo_dir(&self, repo: &ManifestRepo) -> PathBuf {
+        match &repo.location {
             Some(location) => self.top_dir.join(location).join(&repo.name),
             None => self.top_dir.join(&repo.name),
-        };
+        }
+    }
+
+    /// Resolves the specified repo to the revision specified in its manifest
+    /// entry
+    pub fn resolve_repo(&self, repo: &mut ManifestRepo) -> Result<()> {
+        let repo_dir = self.repo_dir(&repo);
 
         if !self.backend.is_repo(&repo_dir) {
             self.backend.clone_repo(&repo.remote, &repo_dir)?;
@@ -45,6 +51,48 @@ impl Workspace {
         self.backend.fetch(&repo_dir)?;
         repo.revision = self.backend.rev_as_hash(&repo_dir, &repo.revision)?;
         self.backend.checkout(&repo.revision, &repo_dir)?;
-        Ok(repo)
+        Ok(())
+    }
+
+    pub fn repo_status(
+        &self,
+        repo: &ManifestRepo,
+        locked_rev: impl AsRef<str>,
+    ) -> Result<(bool, bool)> {
+        let repo_dir = self.repo_dir(&repo);
+
+        let (revision, is_dirty) = match self.backend.is_repo(&repo_dir) {
+            true => (
+                self.backend.get_current_hash(&repo_dir)?.as_str() == locked_rev.as_ref(),
+                self.backend.is_dirty(&repo_dir)?,
+            ),
+            false => bail!("{} is not a valid repo", repo.name),
+        };
+
+        Ok((revision, is_dirty))
+    }
+
+    pub fn repo_run(&self, repo: &ManifestRepo, command: impl AsRef<str>) -> Result<()> {
+        let repo_dir = self.repo_dir(&repo);
+        if !self.backend.is_repo(&repo_dir) {
+            bail!("{} is not a valid repo", repo.name);
+        }
+        match Command::new("sh")
+            .arg("-c")
+            .arg(command.as_ref())
+            .current_dir(&repo_dir)
+            .status()
+        {
+            Ok(status) => {
+                if !status.success() {
+                    bail!("error occurred during command execution");
+                }
+            }
+            Err(_) => {
+                bail!("failed to run command");
+            }
+        };
+
+        Ok(())
     }
 }
