1c081f559cd2cdca94007b3d71eab2e9a4033007
Jorge Gomez <jgomezmail-dev@proton.me>
2026-07-02 15:24:45 -0400

    updating unit tests and comment

--- src/commands/topdir.rs
+++ src/commands/topdir.rs
@@ -5,7 +5,8 @@ use std::path::Path;
 
 /// Walks from the given directory (or the current directory if none is
 /// provided) upwards until either the root is reached or until a
-/// chord.yaml is found.
+/// chord.yaml is found. If not found, the command errors out, makes it
+/// easier for use in shell scripts.
 pub fn run(path: impl AsRef<Path>) -> Result<()> {
     match utils::get_top_dir(&path) {
         Some(dir) => println!("{}", dir.display()),
