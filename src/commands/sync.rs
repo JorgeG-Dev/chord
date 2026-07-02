4e87a92a408830a2f690a1b5e6832125b7d24c6a
Jorge Gomez <jgomezmail-dev@proton.me>
2026-07-01 21:53:43 -0400

    made it so failing a git operation doesn't stop the process

--- src/commands/sync.rs
+++ src/commands/sync.rs
@@ -1,7 +1,7 @@
 //! Contains the logic for performing the Sync command
 use crate::workspace::{Lockfile, Manifest, Workspace};
 
-use anyhow::Result;
+use anyhow::{Result, bail};
 
 /// Attempts to parse the manifest file and sync it to the revisions outlined
 /// in the lockfile, if it exists. If not, a new lockfile is created and each
@@ -24,14 +24,30 @@ pub fn run(workspace: Workspace) -> Result<()> {
 
     // 4. Drain the manifest repos, perform sync operations, and create lockfile
     // struct
+    let mut failed_repos = 0;
+    let total_repos = manifest.repos.len();
     let mut new_lockfile = Lockfile::new();
     for mut repo in manifest.repos.drain(..) {
-        workspace.resolve_repo(&mut repo)?;
-        new_lockfile.insert(repo.name, repo.revision);
+        match workspace.resolve_repo(&mut repo) {
+            Ok(_) => new_lockfile.insert(repo.name, repo.revision),
+            Err(_) => {
+                failed_repos += 1;
+                continue;
+            }
+        }
     }
 
     // 5. Write the new lockfile to disk
     new_lockfile.write(workspace.top_dir())?;
+
+    // 6. Bail with a message if a repo failed to sync
+    if failed_repos > 0 {
+        bail!(
+            "{} out of {} repos failed to sync",
+            failed_repos,
+            total_repos
+        )
+    }
 
     Ok(())
 }
--- src/commands/update.rs
+++ src/commands/update.rs
@@ -1,7 +1,7 @@
 //! Contains the logic for performing the Update command
 use crate::workspace::{Lockfile, Manifest, Workspace};
 
-use anyhow::Result;
+use anyhow::{Result, bail};
 
 /// Syncs the repos in the manifest to the revisions specified in
 /// the manifest, ignoring the lockfile. Generates a new file after
@@ -12,13 +12,29 @@ pub fn run(workspace: Workspace) -> Result<()> {
 
     // 2. Drain the manifest repos, perform update operations, and create lockfile
     // struct
+    let mut failed_repos = 0;
+    let total_repos = manifest.repos.len();
     let mut new_lockfile = Lockfile::new();
     for mut repo in manifest.repos.drain(..) {
-        workspace.resolve_repo(&mut repo)?;
-        new_lockfile.insert(repo.name, repo.revision);
+        match workspace.resolve_repo(&mut repo) {
+            Ok(_) => new_lockfile.insert(repo.name, repo.revision),
+            Err(_) => {
+                failed_repos += 1;
+                continue;
+            }
+        }
     }
 
     // 3. Write the new lockfile to disk
     new_lockfile.write(workspace.top_dir())?;
+
+    // 4. Bail with a message if a repo failed to sync
+    if failed_repos > 0 {
+        bail!(
+            "{} out of {} repos failed to sync",
+            failed_repos,
+            total_repos
+        )
+    }
     Ok(())
 }
