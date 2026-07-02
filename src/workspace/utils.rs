3ffb9ae3b1af762a63d7cb70a7552d8a31405c7c
Jorge Gomez <jgomezmail-dev@proton.me>
2026-07-01 23:24:59 -0400

    refactored util to take start path argument

--- src/workspace/utils.rs
+++ src/workspace/utils.rs
@@ -1,27 +1,17 @@
-use std::{env, path};
+use std::path::{Path, PathBuf};
 
-/// Walks from the current directory up to the root until it either finds
+/// Walks from the given directory up to the root until it either finds
 /// a Chord manifest file or there are no more directories left to check
-pub fn get_top_dir() -> Option<path::PathBuf> {
-    match env::current_dir() {
-        Ok(mut current) => {
-            let mut found = false;
-            loop {
-                if current.join("chord.yaml").exists() {
-                    found = true;
-                    break;
-                }
-
-                // 3. If manifest file doesn't exist, go up one directory
-                if let Some(next) = current.parent() {
-                    current = next.to_path_buf();
-                } else {
-                    break;
-                }
-            }
-
-            if found { Some(current) } else { None }
+pub fn get_top_dir(start: impl AsRef<Path>) -> Option<PathBuf> {
+    let mut current = start.as_ref().to_path_buf();
+    loop {
+        if current.join("chord.yaml").exists() {
+            return Some(current);
         }
-        Err(_) => None,
+
+        match current.parent() {
+            Some(next) => current = next.to_path_buf(),
+            None => return None,
+        }
     }
 }
