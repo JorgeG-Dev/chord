6716181e06daf447af93db048c4e8a2a2e718c87
Jorge Gomez <jgomezmail-dev@proton.me>
2026-07-05 01:56:47 -0400

    adding placeholder manifest file and subcommand functions

---
+++ src/commands/manifest.rs
@@ -0,0 +1,11 @@
+use std::path::PathBuf;
+
+use anyhow::Result;
+
+pub fn add(name: String, remote: String, revision: String, location: PathBuf) -> Result<()> {
+    Ok(())
+}
+
+pub fn remove(name: String) -> Result<()> {
+    Ok(())
+}
