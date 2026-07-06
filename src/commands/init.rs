e85e27f31dd3052848386e24df0bfc2a24451c83
Jorge Gomez <jgomezmail-dev@proton.me>
2026-07-05 21:58:23 -0400

    updating init to use new add_repo function

--- src/commands/init.rs
+++ src/commands/init.rs
@@ -1,10 +1,10 @@
 //! Contains the logic for performing the Init command
-use crate::workspace::{Manifest, ManifestRepo};
+use crate::workspace::Manifest;
 use anyhow::{Result, bail};
 use serde_saphyr;
 use std::fs::File;
 use std::io::ErrorKind;
-use std::path::{Path, PathBuf};
+use std::path::Path;
 
 /// Initializes a chord workspace by creating a chord.yaml
 /// with an example repo pointing at this project's repo
@@ -15,14 +15,13 @@ pub fn run(path: impl AsRef<Path>) -> Result<()> {
     let manifest_dir = path.as_ref().canonicalize()?;
 
     // 2. Create the default configuration
-    let default_manifest = Manifest {
-        repos: vec![ManifestRepo {
-            remote: String::from("https://github.com/JorgeG-Dev/chord.git"),
-            revision: String::from("main"),
-            name: String::from("chord"),
-            location: Some(PathBuf::from(".")),
-        }],
-    };
+    let mut default_manifest = Manifest::new();
+    default_manifest.add_repo(
+        "chord".to_string(),
+        "https://github.com/JorgeG-Dev/chord.git".to_string(),
+        "main".to_string(),
+        None,
+    )?;
 
     // 3. Attempt to create the new file and write the default manifest to it
     let mut manifest_file = match File::create_new(manifest_dir.join("chord.yaml")) {
