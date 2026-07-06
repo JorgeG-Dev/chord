9ce82ce2a9a4c50bdee1134c3601b27340268ffb
Jorge Gomez <jgomezmail-dev@proton.me>
2026-07-05 21:58:33 -0400

    adding modify repo command

--- src/commands/manifest.rs
+++ src/commands/manifest.rs
@@ -2,20 +2,20 @@
 use crate::workspace::{Manifest, Workspace};
 use std::path::PathBuf;
 
-use anyhow::Result;
+use anyhow::{Result, bail};
 
 pub fn add_run(
     name: String,
     remote: String,
     revision: String,
-    location: PathBuf,
+    location: Option<PathBuf>,
     workspace: Workspace,
 ) -> Result<()> {
     // 1. Open and parse the manifest file
     let mut manifest = Manifest::read(workspace.top_dir())?;
 
     // 2. Add the new repo
-    manifest.add_repo(name, remote, revision, Some(location))?;
+    manifest.add_repo(name, remote, revision, location)?;
 
     // 3. Write the new manifest to disk
     manifest.write(workspace.top_dir())?;
@@ -30,6 +30,33 @@ pub fn remove_run(name: String, workspace: Workspace) -> Result<()> {
     manifest.remove_repo(name)?;
 
     // 3. Write the new manifest to disk
+    manifest.write(workspace.top_dir())?;
+    Ok(())
+}
+
+pub fn modify_run(
+    name: String,
+    new_name: Option<String>,
+    new_remote: Option<String>,
+    new_revision: Option<String>,
+    new_location: Option<PathBuf>,
+    workspace: Workspace,
+) -> Result<()> {
+    // 1. Check to see if at least one field was provided
+    if new_name.is_none()
+        && new_remote.is_none()
+        && new_revision.is_none()
+        && new_location.is_none()
+    {
+        bail!("no new values were provided")
+    }
+    // 2. Open and parse the manifest file
+    let mut manifest = Manifest::read(workspace.top_dir())?;
+
+    // 3. Add the new repo
+    manifest.modify_repo(name, new_name, new_remote, new_revision, new_location)?;
+
+    // 4. Write the new manifest to disk
     manifest.write(workspace.top_dir())?;
     Ok(())
 }
